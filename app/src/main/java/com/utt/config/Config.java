b65cb92be291c575e8e36ced3d68a7f380967583
Nhật Huy <38008978+nhathuyit3@users.noreply.github.com>
2021-01-28 10:02:10 +0700

    Update Config.java

--- app/src/main/java/com/utt/config/Config.java
+++ app/src/main/java/com/utt/config/Config.java
@@ -4,6 +4,6 @@ public class Config {
 
     public static final String PAYPAL_CLIENT_ID = "AWKXi0HWWzMgzgLHeWQaddB67wAk3-88_uNRHBA-XTlvwWSv8ftHwuDAt9YL6HZKpcJ7905uIKA0aQac";
     public static final String API_URL = "http://pnstreets.000webhostapp.com/";
-
+//test
 
 }
